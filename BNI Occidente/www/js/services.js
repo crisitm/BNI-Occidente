5087eaad8411974da72f0dad1001a41b38f9b81b
eliasdev <eliasgomez.developer@gmail.com>
2017-02-28 12:56:39 -0600

    profile ready
    working version

--- BNI Occidente/www/js/services.js
+++ BNI Occidente/www/js/services.js
@@ -1,50 +1,121 @@
 angular.module('starter.services', [])
 
-.factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
-
-  // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
-  }];
-
-  return {
-    all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
-        }
-      }
-      return null;
-    }
+.factory('AppService', function($rootScope, $state, $q, $http, $cordovaDevice, $ionicLoading, $cordovaToast) {
+  var global = {
+    appVersion : '0.1',
+    baseURL : 'http://bni.yesduet.com/?E=',
+    device : {},
+    androidSenderID : 'XXXXXXXXX',
+    pushToken : '',
+    members : []
   };
+  global.init = function () {
+    $rootScope.goTo = function(pScreen) {
+      $state.transitionTo( pScreen );
+    };
+    global.initDevice();
+    //global.initPush();
+  };
+  global.initDevice = function () {
+    global.setDevice( $cordovaDevice.getDevice() );
+    var dvc = global.getDevice();
+    var dvcData = global.fetch( "device", { uuid : dvc.uuid, model : dvc.model, platform : dvc.platform, version : dvc.version, appVersion : global.appVersion } );
+    dvcData.then(
+      function(res) {
+        global.setDevice( res.data.device );
+      }, null);
+  };
+  global.start = function () {
+    return global.fetch( "start", {} );
+  };
+  global.saveSettings = function ( pSettings ) {
+    if( pSettings.firstName == 'null' ){
+      pSettings.firstName = String( '' );
+    }
+    if( pSettings.lastName == 'null' ){
+      pSettings.lastName = String( '' );
+    }
+    if( pSettings.phone == 'null' ){
+      pSettings.phone = String( '' );
+    }
+    if( pSettings.email == 'null' ){
+      pSettings.email = '';
+    }
+    return global.fetch( "save-settings", { uuid : global.getDevice().uuid, firstName : pSettings.firstName, lastName : pSettings.lastName, phone : pSettings.phone, email : pSettings.email, push : ( pSettings.getPush? 1 : 0 ), occuppation: pSettings.occuppation } );
+  };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  global.var_dump = function ( pException ) {
+    console.log(pException);
+  };
+  global.toastIt = function ( pText ) {
+    $cordovaToast.showLongCenter( pText );
+  };
+  global.doneLoading = function(){
+    $ionicLoading.hide();
+  };
+  global.loading = function( pMessage, pDuration ) {
+    if( pMessage && ( pMessage.length > 0 ) ){
+      template = pMessage;
+      duration = pDuration;
+    } else{
+      template = '<ion-spinner></ion-spinner>';
+      duration = 30000;
+    }
+    $ionicLoading.show( {
+      template: template,
+      duration: duration
+    });
+  };
+  global.fetch = function ( runFunction, data ) {
+    return $q(function(resolve, reject) {
+      var deferred = $q.defer();
+      setTimeout(function() {
+        deferred.notify('loading');
+        var ThisURL = global.baseURL + runFunction;
+        for( var x in data )	{
+          ThisURL += "&" + encodeURIComponent( x ) + "=" + encodeURIComponent( data[x] );
+        }
+        try {
+          console.log( ThisURL );
+          resolve( $http.get( ThisURL ) );
+        }
+        catch ( e ){
+          reject( e );
+        }
+      }, 100);
+      return deferred.promise;
+    });
+  };
+  global.setPushToken = function ( ptoken ) {
+    global.pushToken = ptoken;
+  };
+  global.getPushToken = function () {
+    return global.pushToken;
+  };
+  global.getUser = function () {
+    return global.user;
+  };
+  global.setUser = function ( pUser ) {
+    global.user = pUser;
+  };
+  global.setDevice = function ( pdevice ) {
+    global.device = pdevice;
+  };
+  global.getDevice = function () {
+    return global.device;
+  };
+  return global;
 });
